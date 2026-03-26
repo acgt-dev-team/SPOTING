666e3939b3d06b616a85afdad77df7fdef240185
mszresal06 <mszresal06@gmail.com>
2026-03-27 02:52:42 +0800

    fix: DO spoting-storage

--- backend/app/utils/spaces.py
+++ backend/app/utils/spaces.py
@@ -1,24 +1,33 @@
 import boto3
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 def get_spaces_client():
     return boto3.client(
         "s3",
         region_name="sgp1",
-        endpoint_url=os.getenv("SPACES_ENDPOINT"),
-        aws_access_key_id=os.getenv("SPACES_KEY"),
-        aws_secret_access_key=os.getenv("SPACES_SECRET"),
+        endpoint_url="https://sgp1.digitaloceanspaces.com",
+        aws_access_key_id=os.getenv("DO_SPACES_KEY"),
+        aws_secret_access_key=os.getenv("DO_SPACES_SECRET"),
     )
 
 
 def upload_text_file(filename: str, content: str):
     client = get_spaces_client()
 
+    bucket = os.getenv("DO_SPACES_BUCKET")
+
+    if not bucket:
+        raise ValueError("DO_SPACES_BUCKET is not set")
+
     client.put_object(
-        Bucket=os.getenv("SPACES_BUCKET"),
+        Bucket=bucket,
         Key=filename,
-        Body=content,
-        ACL="private"
+        Body=content.encode("utf-8"),
+        ACL="public-read"
     )
 
-    return f"{os.getenv('SPACES_ENDPOINT')}/{os.getenv('SPACES_BUCKET')}/{filename}"
+    return f"https://{bucket}.sgp1.digitaloceanspaces.com/{filename}"
