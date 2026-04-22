d6871ef8adacda3124471adcb61afbe101b51cb7
irsyadilham <irsyadmhdilham@gmail.com>
2026-04-22 11:19:21 +0800

    add queue-system

---
+++ queue-system/app/main.py
@@ -0,0 +1,89 @@
+from fastapi import FastAPI, Depends
+from fastapi.requests import Request
+from sqlmodel import Session, select
+from db.config import get_session
+from typing import Annotated
+from db.model.profil_tugasan import ProfilTugasan
+from db.model.tugasan import Tugasan
+from .publish_queue import queue_tugasan
+import requests
+import subprocess
+from datetime import datetime
+import os
+
+app = FastAPI()
+agent_url = os.getenv('AGENT_URL')
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+@app.get('/test')
+def testing():
+    return { 'message': 'API successfully instantiate' }
+
+@app.post('/imbasan', tags=['Tugasan'])
+async def imbasan(request: Request, session: SessionDep):
+    body = await request.json()
+    profil_tugasan_id = body['profil_tugasan_id']
+    penjadualan = body.get('penjadualan')
+
+    profil_tugasan = session.get(ProfilTugasan, profil_tugasan_id)
+    stmt = select(Tugasan).where(Tugasan.id == profil_tugasan.tugasan_id)
+    tugasan = session.exec(stmt).one()
+
+    # Execute now, instantly
+    if not penjadualan:
+        agent_data = {
+            'profil_tugasan_id': profil_tugasan_id,
+            'ip': [tugasan.ip_start, tugasan.ip_end]
+        }
+
+        # Call agent to start scanning
+        try:
+            response = requests.post(f'{agent_url}/mula-imbasan', json=agent_data)
+            res_data = response.json()
+            if res_data['message'] == 'Imbasan bermula':
+                profil_tugasan.status = 2
+                session.add(profil_tugasan)
+                session.commit()
+
+            data = {
+                'mesej': 'berjaya',
+            }
+
+            return data
+        except Exception as e:
+            print('error =', e)
+
+
+# @app.post('/schedule-job', tags=['Tugasan'])
+# async def schedule_job(request: Request):
+#     body = await request.json()
+#     tugasan_id = body['tugasan_id']
+#     schedule = body['jadualkan_pada']
+
+#     dt = datetime.strptime(schedule, '%Y-%m-%d %H:%M:%S')
+#     day = dt.day
+#     month = dt.month
+#     hour = dt.hour
+#     minute = dt.minute
+
+#     cron = f'{minute} {hour} {day} {month} *'
+
+#     new_job = f'{cron} curl http://localhost:8010/agct-agent/{tugasan_id}'
+
+#     # Get existing cronjob
+#     existing = subprocess.run(['crontab', '-l'], capture_output=True, text=True)
+
+#     cron_content = existing.stdout if existing.returncode == 0 else ''
+
+#     # Add new job
+#     cron_content += '\n' + new_job + '\n'
+
+#     # Write back
+#     subprocess.run(['crontab', '-'], input=cron_content, text=True)
+
+#     data = {
+#         'tugasan_id': tugasan_id,
+#         'status': -1
+#     }
+#     return data
---
+++ queue-system/worker/main.py
@@ -0,0 +1,61 @@
+import pika
+from sqlmodel import Session
+from db.config import engine
+from db.model.imbasan import Imbasan
+from db.model.profil_tugasan import ProfilTugasan
+import json
+from datetime import datetime, timezone
+
+credentials = pika.PlainCredentials(username='admin', password='strongpassword')
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(
+        host='178.128.116.198',
+        port=5672,
+        credentials=credentials
+    )
+)
+
+channel = connection.channel()
+
+channel.queue_declare(queue='tugasan_queue')
+
+def callback(ch, method, properties, body):
+
+    body_str = body.decode('utf-8')
+
+    body = json.loads(body_str)
+
+    profil_tugasan_id = body['profil_tugasan_id']
+    data_imbasan = body['data_imbasan']
+
+    hasil_imbasan = Imbasan(
+        x_profil_tugasan_id=profil_tugasan_id,
+        data_imbasan=data_imbasan
+    )
+
+    with Session(engine) as session:
+        session.add(hasil_imbasan)
+        session.commit()
+        session.refresh(hasil_imbasan)
+
+    obj = session.get(ProfilTugasan, profil_tugasan_id)
+    obj.selesai_pada = datetime.now(timezone.utc)
+    session.add(obj)
+    session.commit()
+
+    print('done update x_profil_tugasan id = ', obj.id)
+
+channel.basic_consume(
+    queue='tugasan_queue',
+    on_message_callback=callback,
+    auto_ack=True
+)
+
+try:
+
+    channel.start_consuming()
+
+except KeyboardInterrupt:
+
+    channel.stop_consuming()
