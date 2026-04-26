591ce296d9af9e23dfea925a4d443b99be5df704
irsyadilham <irsyadmhdilham@gmail.com>
2026-04-26 13:51:52 +0800

    update

--- rabbitmq/worker/db/model/tugasan.py
+++
@@ -1,8 +0,0 @@
-from sqlmodel import SQLModel, Field
-
-class Tugasan(SQLModel, table=True):
-    __tablename__ = 'tugasan'
-
-    id: int = Field(primary_key=True)
-    ip_start: str
-    ip_end: str
