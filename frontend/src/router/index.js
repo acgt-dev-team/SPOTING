aaa39a969ee0ec6f289c509803c1a1b52cf52206
mszresal06 <mszresal06@gmail.com>
2026-05-13 09:09:51 +0800

    update

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router"
+import { createRouter, createWebHashHistory } from "vue-router"
 import { setupGuards } from "../router/guards"
 
 // =========================
@@ -89,7 +89,7 @@ const routes = [
 // ROUTER INIT
 // =========================
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
