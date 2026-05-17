cf508d34427411e1b26afea5b5e7b04166c77828
irsyadilham <irsyadmhdilham@gmail.com>
2026-05-17 14:48:26 +0800

    Remove User API

--- scanning-api/api/main.py
+++ scanning-api/api/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
-from api.controllers import agent, user
+from api.controllers import agent
 
 app = FastAPI()
 
 app.include_router(agent.router)
-app.include_router(user.router)
