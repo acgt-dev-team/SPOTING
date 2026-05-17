cbb4fbe180ba5b9c755e516daaec553e1e038ea9
irsyadilham <irsyadmhdilham@gmail.com>
2026-05-17 13:49:56 +0800

    Add more details for SwaggerUI

--- scanning-api/api/controllers/agent.py
+++ scanning-api/api/controllers/agent.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Request, HTTPException
 from sqlmodel import select
 from db.model.tugasan import Tugasan
-from db.model.ejen import Ejen
+from db.model.ejen import Ejen, EjenInit, EjenInitBody, EjenHasil, EjenHasilResponse
 from db.model.profil_tugasan import ProfilTugasan
 from db.config import SessionDep
 import os
@@ -9,11 +9,9 @@ import os
 router = APIRouter(prefix='/ejen')
 platform_url = os.getenv('PLATFORM_URL')
 
-@router.get('/init', tags=['Ejen'])
-async def agent_init(session: SessionDep, request: Request):
-    body = await request.json()
-
-    host_ip = body['host_ip']
+@router.post('/init', tags=['Ejen'], response_model=EjenInit)
+async def agent_init(body: EjenInitBody, session: SessionDep):
+    host_ip = body.host_ip
     stmt = select(Tugasan).where(
         Tugasan.ip_start <= host_ip,
         Tugasan.ip_end >= host_ip
@@ -28,14 +26,13 @@ async def agent_init(session: SessionDep, request: Request):
     session.add(ejen)
     session.commit()
 
-    return platform_url
+    return {
+        'platform_url': platform_url
+    }
 
-@router.get('/penjadualan', tags=['Ejen'])
-async def agent_penjadualan(session: SessionDep, request: Request):
+@router.get('/penjadualan', tags=['Ejen'], response_model=ProfilTugasan)
+async def agent_penjadualan(host_ip: str, session: SessionDep):
     # Looking for x_profil
-    body = await request.json()
-    host_ip = body['host_ip']
-
     ejen = session.exec(select(Ejen).where(Ejen.ip_address == host_ip)).one()
 
     profil_tugasan = session.exec(select(ProfilTugasan).where(
@@ -45,12 +42,10 @@ async def agent_penjadualan(session: SessionDep, request: Request):
 
     return profil_tugasan
 
-@router.put('/hasil', tags=['Ejen'])
-async def hasil_imbasan(request: Request, session: SessionDep):
-    body = await request.json()
-
-    hasil_imbasan = body['hasil_imbasan']
-    host_ip = body['host_ip']
+@router.post('/hasil', tags=['Ejen'], response_model=EjenHasilResponse)
+async def hasil_imbasan(body: EjenHasil, session: SessionDep):
+    hasil_imbasan = body.hasil_imbasan
+    host_ip = body.host_ip
 
     try:
         stmt = select(Ejen).where(Ejen.ip_address == host_ip)
--- scanning-api/db/model/ejen.py
+++ scanning-api/db/model/ejen.py
@@ -1,5 +1,20 @@
 from sqlmodel import SQLModel, Field, Column
 from sqlalchemy.dialects.postgresql import INET, JSON
+from pydantic import BaseModel, Field as PyField
+
+class EjenInit(BaseModel):
+    platform_url: str = PyField(examples=['http://ec2-56-68-97-215.ap-southeast-5.compute.amazonaws.com/'])
+
+class EjenInitBody(BaseModel):
+    host_ip: str = PyField(examples=['127.0.0.1'])
+
+class EjenHasil(BaseModel):
+    host_ip: str = PyField(examples=['127.0.0.1'])
+    hasil_imbasan: dict = PyField(examples=['JSON string'])
+
+class EjenHasilResponse(BaseModel):
+    status: int
+    message: str
 
 class Ejen(SQLModel, table=True):
     id: int = Field(primary_key=True)
