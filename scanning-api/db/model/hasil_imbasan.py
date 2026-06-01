0a939dab158be9e4dc3d2d9c0c6787113cb0955a
irsyadilham <irsyadmhdilham@gmail.com>
2026-06-01 09:27:31 +0800

    Restructure

--- scanning-api/db/model/hasil_imbasan.py
+++ scanning-api/db/model/hasil_imbasan.py
@@ -7,20 +7,7 @@ class HasilImbasan(SQLModel, table=True):
     __tablename__ = "hasil_imbasan"
 
     id: int = Field(primary_key=True)
-
-    profil_tugasan_id: int = Field(
-        foreign_key='x_profil_tugasan.id'
-    )
-
-    ejen_id: int = Field(
-        foreign_key='ejen.id'
-    )
-
-    created_at: datetime = Field(
-        default=datetime.now()
-    )
-
-    hasil: list[dict] = Field(
-        sa_column=Column(JSON),
-        default_factory=list
-    )
+    profil_tugasan_id: int = Field(foreign_key='x_profil_tugasan.id')
+    ejen_id: int = Field(foreign_key='ejen.id')
+    created_at: datetime = Field(default=datetime.now())
+    hasil: list[dict] = Field(sa_column=Column(JSON), default_factory=list)
